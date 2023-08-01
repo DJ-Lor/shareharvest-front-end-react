3dfaabf6c08d3d39b8f4ff960729ffd867c8a1ed
Loreli <dejesus.loreli@gmail.com>
2023-08-02 09:04:36 +1000

    upload widget multiple images

--- src/components/UploadWidget.jsx
+++ src/components/UploadWidget.jsx
@@ -1,14 +1,12 @@
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
-import axios from "axios"
-import { useEffect, useRef, useState } from "react"
-
-export default function UploadWidget(){
-  const cloudinaryRef = useRef()
-  const widgetRef = useRef()
-  const [uploaded, setUploaded] = useState(false)
-  const [listingImageUrls, setListingImageUrls] = useState([])
-  const [fileImageNames, setFileImageNames] = useState([])
-
+export default function UploadWidget() {
+  const cloudinaryRef = useRef();
+  const widgetRef = useRef();
+  const [uploaded, setUploaded] = useState(false);
+  const [listingImageUrls, setListingImageUrls] = useState([]);
+  const [fileImageNames, setFileImageNames] = useState([]);
 
   useEffect(() => {
     cloudinaryRef.current = window.cloudinary;
@@ -18,36 +16,36 @@ export default function UploadWidget(){
         uploadPreset: "shareharvestuploadpreset",
       },
       function (error, result) {
-        if (!error && result && result.event === "success") {
+        console.log(result.event);
+        if (!error && result && result.event === "queues-end") {
+          console.log(result);
           // Check if 'info' and 'files' properties exist before accessing them
           const files = result.info?.files || [];
           // Create an array of URLs from the files list
           const imageUrls = files.map((file) => file.uploadInfo?.url);
           // Set the state with the array of URLs
           setListingImageUrls(imageUrls);
-  
+
           // Create an array of image names
           const imageNames = files.map((file) => file.name);
           setFileImageNames(imageNames);
-  
+
           // Save the image name to MongoDB Atlas here
           // saveImageToMongoAtlas(result.info.original_filename);
           setUploaded(true);
         }
-        console.log(result);
-        console.log(fileImageNames);
-        console.log(listingImageUrls);
+        // console.log(result);
+        // console.log(fileImageNames);
+        // console.log(listingImageUrls);
       }
     );
   }, []);
 
   useEffect(() => {
     // This effect will run after each render and will log the state values
-    console.log("Updated File Image Names:", fileImageNames);
-    console.log("Updated Listing Image URLs:", listingImageUrls);
+    // console.log("Updated File Image Names:", fileImageNames);
+    // console.log("Updated Listing Image URLs:", listingImageUrls);
   }, [fileImageNames, listingImageUrls]); // Add fileImageNames and listingImageUrls as dependencies
-  
-  
 
   function onClickUpload() {
     widgetRef.current.open();
@@ -67,24 +65,28 @@ export default function UploadWidget(){
   //     console.error("Error saving image name to MongoDB:", error);
   //   }
   // }
-  
-  return(
+
+  return (
     <div className="bg-light border border-gray-300 w-full p-4 rounded-lg text-center flex justify-center">
-    <button 
-    type="submit"
-    onClick={onClickUpload}
-    className="bg-gray-500 text-light p-2 flex justify-center rounded-lg"
-    pill
-    >
-      Upload photo
-    </button>
-    {uploaded && <p className="text-greenc flex text-center items-center">Uploaded successfully! Image Name: {fileImageNames}</p>}
-  </div>
-  )
+      <button
+        type="submit"
+        onClick={onClickUpload}
+        className="bg-gray-500 text-light p-2 flex justify-center rounded-lg"
+        pill
+      >
+        Upload photo
+      </button>
+      {uploaded && (
+        <p className="text-greenc flex text-center items-center">
+          Uploaded successfully! Image Name: {fileImageNames}
+        </p>
+      )}
+    </div>
+  );
 }
 
-//uploaded successfully should be based off something else, not click 
+//uploaded successfully should be based off something else, not click
 
-// update the file name to reflect once uploaded in view 
+// update the file name to reflect once uploaded in view
 // edit the file name with added code for uniqueness before saving to atlas
-// display the image in view end point 
+// display the image in view end point
