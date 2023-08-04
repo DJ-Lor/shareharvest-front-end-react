73cc18b3d51b8cfb5d21f7a7f01228f6501dd88e
Loreli <dejesus.loreli@gmail.com>
2023-08-04 13:42:33 +1000

    new user profile postcode drop down

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -111,7 +111,7 @@ export default function SignUp() {
                 </label>
                 <select
                   id="countries"
-                className="bg-gray-100 text-gray-400 border
+                className="bg-gray-50 text-gray-700 border
                 border-gray-300 text-sm rounded-lg
                 focus:ring-blue-500 focus:border-blue-500 
                 block w-full p-2.5 required"
